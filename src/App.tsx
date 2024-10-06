9d86b84431098b2839c2e7d3bd318bf31e18ff72
ComboRush <comborush.init@proton.me>
2024-10-06 21:27:53 +0200

    added two more routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { AnimatePresence } from "framer-motion";
 import AccountSettingsPage from "./pages/account/settings/index.tsx";
 import AccountIdPage from "./pages/account/[accountId]/index.tsx";
 import ChatIdPage from "./pages/chat/[chatid]/index.tsx";
+import LoginPage from "./pages/auth/login/index.tsx";
+import SignupPage from "./pages/auth/signup/index.tsx";
 import AccountPage from "./pages/account/index.tsx";
 import NotFoundPage from "./pages/not-found.tsx";
 import AboutPage from "./pages/about/index.tsx";
@@ -13,6 +15,11 @@ import ChatPage from "./pages/chat/index.tsx";
 import MainPage from "./index.tsx";
 
 import Navbar from "./components/navbar.tsx";
+
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.SUPABASE_URL) throw new Error("Missing SUPABASE_URL")  
+  if (!process.env.SUPABASE_KEY) throw new Error("Missing SUPABASE_KEY")
+}
 
 function App() {
   return (
@@ -28,6 +35,8 @@ function App() {
             <Route path="chat" element={<ChatPage />}>
               <Route path=":chatId" element={<ChatIdPage />} />
             </Route>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignupPage />} />
             <Route path="dashboard" element={<ChatPage />} />
             <Route path="account" element={<AccountPage />}>
               <Route path="settings" element={<AccountSettingsPage />} />
